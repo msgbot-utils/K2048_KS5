6adb4dc529fbb7048b152b3c6411fd58719e37d7
semi703 <156795575+semi703@users.noreply.github.com>
2024-05-30 19:47:01 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,30 +1,30 @@
-const KS5 = require("./KS5/index")
+const scriptName = "2048";
 
+const K2048 = require("K2048");
+
+const a = ";"; //접두사
 let playing = {}
-let waiting = {}
-/**
- * @param {string} room
- * @param {string} msg
- * @param {string} sender
- * @param {boolean} isGC
- * @param {object} replier
- * @param {function} replier.reply
- * @param {object} iDB
- * @param {function(): number} iDB.getProfileHash
- * @param {string} pN
- */
-function response(room, msg, sender, isGC, replier, iDB, pN) { //굳이 모듈 써야함?  나중에 미니게임 월드에 넣을거시빈  오목도 넣고 할려그
-    let hash = iDB.getProfileHash()
 
-    //테스트용 명령어
-    if(msg == ";오목 솔로"){
-        if(playing[hash]) return replier.reply("이미 오목을 시작했어요!")
+function send(room, replier, msg){
+  replier.reply("2️⃣0️⃣4️⃣8️⃣\n\n"+playing[room]+"\n\n점수: "+playing[room].score+"\n\nw: 위로/s: 아래로\na:왼쪽으로/d:오른쪽으로"+(msg?"\n\n"+msg:""));
+}
 
-        let game = new KS5({name: sender, hash: hash}, {name: sender, hash: hash})
-        playing[hash] = {
-
-        }
-        
-        replier.reply("솔로 오목을 시작할게요! (테스트용)")
+function response(room, msg, sender, isGroupChat, replier, imageDB, packageName) {
+  if(msg == a+"2048 시작"){
+    if(playing[room]) return send(room, replier, "이미 플레이 중이에요!");
+    playing[room] = new K2048(4);
+    send(room, replier, "2048 게임을 시작합니다!");
+  }
+  
+  if(["w", "a", "s", "d"].includes(msg)){
+    if(playing[room]){
+      playing[room].input(msg);
+      if(playing[room].isEnd){
+        send(room, replier, "게임 오버!");
+        delete playing[room];
+      }else{
+        send(room, replier);
+      }
     }
+  }
 }
